f9ca3d2b9bf93f065568d7060e0a692702be3e9f
Paul S. Schweigert <paul@paulschweigert.com>
2025-03-10 20:23:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# github-activity
+
+This script retrieves the GitHub activity for a specified user and prints it to the console.
+
+## Usage
+
+To use this script, you will need to install the `requests`  library. You can do this by running the following command in your terminal:
+
+```bash
+pip install requests
+```
+
+Once you have installed the `requests` library, you can run the script by providing the GitHub username as a command-line argument. For example, to retrieve the GitHub activity for the user `example-user`, you would run the following command:
+
+```bash
+python github-activity.py example-user
+```
+
+The script will then retrieve the GitHub activity for the specified user and print it to the console.
+
+By default, the script will pull activity for the previous seven days. To choose a different date range, pass a start date and (optional) end date to the script.
+
+```bash
+python github-activity.py example-user 2024-01-01 2024-01-31
+```
+
+## Example Output
+
+Here is an example of the output that the script might produce:
+
+```
+Getting all activiting between 2024-01-01T00:00:00Z and 2024-01-31T00:00:00Z for example-user
+PRs:
+* awesome/coder
+    - feat(coder): create new coder: https://github.com/awesome/coder/pull/789
+Reviews:
+* sample/repo
+    - adding contributing guidelines for demo: https://github.com/sample/repo/pull/283
+Issues:
+* some-org/some-repo
+    - some issue: https://github.com/some-org/some-repo/issue/123
+```
+
+## License
+This script is licensed under the Apache License, Version 2.0. You can find a copy of the license in the `LICENSE` file in the root directory of this repository.
---
+++ github-activity.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python3
+# Assisted by watsonx Code Assistant
+from datetime import datetime, timedelta
+import json
+import os
+import requests
+import sys
+
+
+def run_github_query(username, start_dt, end_dt):
+    url = "https://api.github.com/graphql"
+
+    query = f"""
+    query {{
+        user(login: "{username}") {{
+            contributionsCollection(from: "{start_dt}", to: "{end_dt}") {{
+              issueContributions (first: 100) {{ 
+                edges {{ 
+                    node {{ 
+                        occurredAt
+                        issue {{
+                            title
+                            url
+                            repository {{
+                                nameWithOwner
+                            }}
+                        }}
+                    }}
+                }}
+              }}
+              pullRequestContributions (first: 100) {{
+                edges {{ 
+                    node {{ 
+                        occurredAt
+                        pullRequest {{ 
+                            title 
+                            url
+                            repository {{
+                                nameWithOwner
+                            }}
+                        }}
+                    }}
+                }}
+              }}
+              pullRequestReviewContributions (first: 100) {{ 
+                edges {{
+                    node {{
+                        occurredAt
+                        pullRequest {{
+                          title
+                          url
+                          repository {{
+                              nameWithOwner
+                          }}
+                        }}
+                    }}
+                }}
+              }}
+          }}
+       }}
+    }}
+    """
+
+    headers = {
+        "Authorization": f"token {os.environ['GITHUB_TOKEN']}",
+        "Content-Type": "application/json",
+    }
+
+    response = requests.post(url, json={"query": query}, headers=headers)
+    return response.json()
+
+def output_contributions(data):
+    issues = data["data"]["user"]["contributionsCollection"]["issueContributions"][
+        "edges"
+    ]
+    prs = data["data"]["user"]["contributionsCollection"]["pullRequestContributions"][
+        "edges"
+    ]
+    reviews = data["data"]["user"]["contributionsCollection"][
+        "pullRequestReviewContributions"
+    ]["edges"]
+
+    # Return PRs
+    if prs:
+        print("Pull Requests:")
+        output = {}
+        for pr in prs:
+            repo = pr["node"]["pullRequest"]["repository"]["nameWithOwner"]
+            title = pr["node"]["pullRequest"]["title"]
+            url = pr["node"]["pullRequest"]["url"]
+            if repo in output.keys():
+                output[repo].append({"title": title, "url": url})
+            else:
+                output[repo] = [{"title": title, "url": url}]
+
+        for key in output.keys():
+            print(f"* {key}")
+            for i in output[key]:
+                print(f"    - {i["title"]}: {i["url"]}")
+
+    # Return Reviews
+    if reviews:
+        print("Reviews:")
+        output = {}
+        for review in reviews:
+            repo = review["node"]["pullRequest"]["repository"]["nameWithOwner"]
+            title = review["node"]["pullRequest"]["title"]
+            url = review["node"]["pullRequest"]["url"]
+            if repo in output.keys():
+                output[repo].append({"title": title, "url": url})
+            else:
+                output[repo] = [{"title": title, "url": url}]
+
+        for key in output.keys():
+            print(f"* {key}")
+            for i in output[key]:
+                print(f"    - {i["title"]}: {i["url"]}")
+
+    # Return Issues
+    if issues:
+        print("Issues:")
+        output = {}
+        for issue in issues:
+            repo = issue["node"]["issue"]["repository"]["nameWithOwner"]
+            title = issue["node"]["issue"]["title"]
+            url = issue["node"]["issue"]["url"]
+            if repo in output.keys():
+                output[repo].append({"title": title, "url": url})
+            else:
+                output[repo] = [{"title": title, "url": url}]
+
+        for key in output.keys():
+            print(f"* {key}")
+            for i in output[key]:
+                print(f"    - {i["title"]}: {i["url"]}")
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print(f"INFO: Assuming github user ID is {os.getlogin}")
+        username = os.getlogin()
+
+    if len(sys.argv) > 2:
+        start_dt = sys.argv[2] + "T00:00:00Z"
+    else:
+        print("INFO: No start date provided, will look back seven days from today")
+        start_dt = (
+            datetime.today().date() - timedelta(days=7)
+        ).isoformat() + "T00:00:00Z"
+
+    if len(sys.argv) > 3:
+        end_dt = sys.argv[3]
+    else:
+        print("INFO: No end date provided, so will including everying up to... NOW")
+        end_dt = (
+            datetime.today().date() + timedelta(days=1)
+        ).isoformat() + "T00:00:00Z"
+
+    print(f"Getting all activiting between {start_dt} and {end_dt} for {username}\n\n")
+
+    data = run_github_query(username, start_dt, end_dt)
+
+    output_contributions(data)
