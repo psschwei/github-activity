3f8c82a573493c47d4c17fbdbdc81dc59c863bf0
Michael Maximilien <mmaximilien@gmail.com>
2025-03-24 08:56:56 -0700

    added --last-week and --this-week options to make it easy to get activities

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,8 @@ var StartDate string
 var EndDate string
 var Username string
 var Token string
+var LastWeek bool
+var ThisWeek bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -23,6 +25,12 @@ var rootCmd = &cobra.Command{
 	Short: "Get your Github activity",
 	Long:  `Get PRs, reviews, and issues created during a specific time interval.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if LastWeek == true {
+			StartDate, EndDate = utils.GetLastWeekDates()
+		} else if ThisWeek == true {
+			StartDate, EndDate = utils.GetThisWeekDates()
+		}
+
 		return github.GetGithubActivity(Domain, StartDate, EndDate, Username, Token)
 	},
 }
@@ -40,6 +48,8 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&Domain, "domain", "d", "github.com", "Github domain")
 	rootCmd.PersistentFlags().StringVarP(&StartDate, "start", "s", utils.GetDefaultStartDate(), "Collect activities starting on this date")
 	rootCmd.PersistentFlags().StringVarP(&EndDate, "end", "e", utils.GetDefaultEndDate(), "Collect activities up to this date")
+	rootCmd.PersistentFlags().BoolVarP(&LastWeek, "last-week", "l", false, "Collect activities for last week (last week Monday to last week Friday")
+	rootCmd.PersistentFlags().BoolVarP(&ThisWeek, "this-week", "n", false, "Collect activities for this week (Monday to Friday")
 	rootCmd.PersistentFlags().StringVarP(&Username, "user", "u", utils.GetCurrentUsername(), "Username")
 	rootCmd.PersistentFlags().StringVarP(&Token, "token", "t", "", "Github Personal Access Token (default `$GITHUB_TOKEN`)")
 }
