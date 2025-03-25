1706a35d777f8f6cd7d9af9612a40949eea9cbbf
dr.max <mmaximilien@gmail.com>
2025-03-25 10:28:04 -0700

    added various options and new formatting (#2)
    * added --last-week and --this-week options to make it easy to get activities
    
    * added --today option and (no) to PRs, Reviews, and Issues report
    
    * Update pkg/utils/utils.go
    
    Co-authored-by: Paul Schweigert <paul@paulschweigert.com>
    
    * Update pkg/utils/utils.go
    
    Co-authored-by: Paul Schweigert <paul@paulschweigert.com>
    
    * added Totals summary at the end of report
    
    ---------
    
    Co-authored-by: Paul Schweigert <paul@paulschweigert.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -22,6 +22,48 @@ func GetDefaultEndDate() string {
 	return time.Now().In(time.Local).AddDate(0, 0, 1).Format("2006-01-02")
 }
 
+func GetTodayDates() (string, string) {
+	now := time.Now()
+	start := now.In(time.Local).Format("2006-01-02")
+	end := now.In(time.Local).AddDate(0,now.Hour(), 0).Format("2006-01-02")
+	return start, end
+}
+
+func GetThisWeekDates() (string, string) {
+	now := time.Now()
+	weekday := time.Duration(now.Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	thisMonday := now.Add(-1 * (weekday - 1) * 24 * time.Hour)
+	thisFriday := thisMonday.Add(5 * 24 * time.Hour)
+
+    thisMonday = time.Date(thisMonday.Year(), thisMonday.Month(), thisMonday.Day(), 0, 0, 0, 0, thisMonday.Location())
+    thisFriday = time.Date(thisFriday.Year(), thisFriday.Month(), thisFriday.Day(), 0, 0, 0, 0, thisFriday.Location())
+
+	return thisMonday.Format("2006-01-02"), thisFriday.Format("2006-01-02")
+}
+
+func GetLastWeekDates() (string, string) {
+	now := time.Now()
+	weekday := now.Weekday()
+	daysToSubtract := 0
+
+	if weekday == time.Sunday {
+		daysToSubtract = 6
+	} else {
+		daysToSubtract = int(weekday) - int(time.Monday) + 7
+	}
+
+	lastMonday := now.AddDate(0, 0, -daysToSubtract)
+	lastFriday := lastMonday.Add(5 * 24 * time.Hour)
+
+    lastMonday = time.Date(lastMonday.Year(), lastMonday.Month(), lastMonday.Day(), 0, 0, 0, 0, lastMonday.Location())
+    lastFriday = time.Date(lastFriday.Year(), lastFriday.Month(), lastFriday.Day(), 0, 0, 0, 0, lastFriday.Location())
+
+	return lastMonday.Format("2006-01-02"), lastFriday.Format("2006-01-02")
+}
+
 func FormatDate(input string) (string, error) {
 	layout := "2006-01-02"
 	t, err := time.Parse(layout, input)
