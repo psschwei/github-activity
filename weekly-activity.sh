256cc857b62eb93ace6d5cbbfc648e9918472f41
Paul Schweigert <paul@paulschweigert.com>
2025-06-10 10:06:46 -0400

    Update weekly-activity.sh

--- weekly-activity.sh
+++ weekly-activity.sh
@@ -6,8 +6,8 @@ which gdate > /dev/null && date_cmd="gdate" || date_cmd="date"
 starting_date=$($date_cmd -dlast-monday +"%Y-%m-%d")
 gh_search_flags="--updated >=$starting_date --json repository,title,url"
 
-echo "PRs ($(gh search prs $gh_search_flags | jq 'length'))"
-gh search prs $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+echo "PRs ($(gh search prs --author @me $gh_search_flags | jq 'length'))"
+gh search prs --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
 
 echo "Reviews ($(gh search prs --reviewed-by @me $gh_search_flags | jq 'length'))"
 gh search prs --reviewed-by @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
