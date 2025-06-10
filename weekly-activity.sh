672697351174ca5c8eb2774c4f72de98f623b0e5
Alex Bozarth <ajbozart@us.ibm.com>
2025-06-10 09:18:47 -0500

    Update GH CLI script (#6)
    * Update GH CLI script
    
    Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>
    
    * Update weekly-activity.sh
    
    * Update weekly-activity.sh
    
    * Update weekly-activity.sh
    
    * Update weekly-activity.sh
    
    ---------
    
    Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>
    Co-authored-by: Paul Schweigert <paul@paulschweigert.com>

--- weekly-activity.sh
+++ weekly-activity.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 set -e
 
-starting_date=$(date -d "7 days ago" +"%Y-%m-%d")
+which gdate > /dev/null && date_cmd="gdate" || date_cmd="date"
 
-# PRs
-gh search prs --author "@me" --created ">=$starting_date"
+starting_date=$($date_cmd -dlast-monday +"%Y-%m-%d")
+gh_search_flags="--updated >=$starting_date --json repository,title,url"
 
-# Reviews
-gh search prs --reviewed-by "@me" --created ">=$starting_date"
+echo "PRs ($(gh search prs --author @me $gh_search_flags | jq 'length'))"
+gh search prs --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
 
-# Issues
-gh search issues --author "@me" --created ">=$starting_date"
+echo "Reviews ($(gh search prs --reviewed-by @me $gh_search_flags | jq 'length'))"
+gh search prs --reviewed-by @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+
+echo "Issues ($(gh search issues --author @me $gh_search_flags | jq 'length'))"
+gh search issues --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+
