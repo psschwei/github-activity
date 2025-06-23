a8a3e454302b42214c470208cfa5538c98cec04b
Alex Bozarth <ajbozart@us.ibm.com>
2025-06-23 09:48:16 -0500

    update gh cli script to filter pr reviews
    Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>

--- weekly-activity.sh
+++ weekly-activity.sh
@@ -10,7 +10,7 @@ echo "PRs ($(gh search prs --author @me $gh_search_flags | jq 'length'))"
 gh search prs --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
 
 echo "Reviews ($(gh search prs --reviewed-by @me $gh_search_flags | jq 'length'))"
-gh search prs --reviewed-by @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+gh search prs --reviewed-by @me -- -author:@me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
 
 echo "Issues ($(gh search issues --author @me $gh_search_flags | jq 'length'))"
 gh search issues --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
