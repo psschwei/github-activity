20c07a6b6fc542719857f300824d89ff323a88c8
Alex Bozarth <ajbozart@us.ibm.com>
2025-06-25 17:38:39 -0500

    Rework weekly-activity.sh to parity with tool
    Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>

--- weekly-activity.sh
+++ weekly-activity.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+# USAGE: To get enterprise activity:
+#   GH_HOST=github.ibm.com ./weekly-activity.sh
+
 set -e
 
 which gdate > /dev/null && date_cmd="gdate" || date_cmd="date"
@@ -6,12 +10,31 @@ which gdate > /dev/null && date_cmd="gdate" || date_cmd="date"
 starting_date=$($date_cmd -dlast-monday +"%Y-%m-%d")
 gh_search_flags="--updated >=$starting_date --json repository,title,url"
 
-echo "PRs ($(gh search prs --author @me $gh_search_flags | jq 'length'))"
-gh search prs --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+prs_cmd="gh search prs --author @me $gh_search_flags"
+reviewed_cmd="gh search prs --reviewed-by @me $gh_search_flags"
+issues_cmd="gh search issues --author @me $gh_search_flags"
 
-echo "Reviews ($(gh search prs --reviewed-by @me $gh_search_flags | jq 'length'))"
-gh search prs --reviewed-by @me -- -author:@me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+prs_len=$($prs_cmd | jq 'length')
+reviews_len=$($reviewed_cmd -- -author:@me | jq 'length')
+issues_len=$($issues_cmd | jq 'length')
 
-echo "Issues ($(gh search issues --author @me $gh_search_flags | jq 'length'))"
-gh search issues --author @me $gh_search_flags --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+echo "${GH_HOST:=github.com} activity since $starting_date"
+echo
 
+if [[ $prs_len > 0 ]]; then
+  echo "PRs ($prs_len)"
+  $prs_cmd --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+fi
+
+if [[ $reviews_len > 0 ]]; then
+echo "Reviews ($reviews_len)"
+$reviewed_cmd --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}' -- -author:@me
+fi
+
+if [[ $issues_len > 0 ]]; then
+echo "Issues ($issues_len)"
+$issues_cmd --template '{{ range . }}{{ printf "* [%s] %s: %s\n" .repository.nameWithOwner .title .url }}{{ end }}'
+fi
+
+echo
+echo "Totals: PRs($prs_len) Reviews($reviews_len) Issues($issues_len)"
